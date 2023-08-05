8ca009fef407dcd1780d0310d91f372419760c90
Red <red.sarouf2014@gmail.com>
2023-08-05 04:09:22 -0400

    add python translating from orm to sqlalchemy lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,36 @@
 from models import Dog
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base, Dog
+from testing.conftest import SQLITE_URL
+engine = create_engine(SQLITE_URL)
+Base.metadata.create_all(engine)
+Session = sessionmaker(engine)
+session = Session()
 
-def create_table(base):
-    pass
+
+def create_table(base, engine):
+    if __name__ == "__main__":
+        engine = create_engine(SQLITE_URL)
+        base.metadata.create.all(engine)
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
+    
 
 def get_all(session):
-    pass
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter(Dog.name == name).first()
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter(Dog.id == id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter(Dog.name == name, Dog.breed == breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    session.query(Dog).update({Dog.breed : breed})
+    session.commit()
